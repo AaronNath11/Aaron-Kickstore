a65d66aabd38c6af1759b5c42e6bb40507a0cd4f
AaronNath11 <aaronsuhaendi@gmail.com>
2025-09-08 16:49:32 +0700

    Test

---
+++ aaron_kickstore/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aaron_kickstore project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aaron_kickstore.settings')
+
+application = get_wsgi_application()
