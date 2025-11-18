6f349fc058350cea5e45e7b96b42cf1e9c91ce37
AaronNath11 <aaronsuhaendi@gmail.com>
2025-11-18 21:37:27 +0700

    Tugas 9

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main import views
-from main.views import show_main, create_product, show_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product, show_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id, proxy_image, create_product_flutter
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
@@ -23,4 +23,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('product/<uuid:id>/edit', edit_product, name='edit_product'),
     path('create-product-ajax', add_product_entry_ajax, name='add_product_entry_ajax'),
-]   
+    path('proxy-image/', proxy_image, name='proxy_image'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+] 
